1baeb1f76d0405436903ce7b50db66be5754ee3b
RobertoBarbagallo <roberto-barbagallo@live.it>
2021-07-22 13:09:33 +0200

    controller per la ricerca del guest, reindirizamento su pagina di ricerca avanzata- inizia parte di Vue

---
+++ resources/js/components/GuestSearch.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+   export default {
+    name: "GuestSearch",
+    props: {
+        name: String,
+        cover_img_path: String,
+    },
+    computed: {
+
+    }
+};
+</script>
