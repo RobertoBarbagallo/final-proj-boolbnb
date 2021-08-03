01be87927f6a1e9e3d565892339d582295e45e54
anto991nio <antonio.91.cardone@gmail.com>
2021-08-03 09:40:40 +0200

    add slider

--- resources/js/components/StructuresSponsored.vue
+++ resources/js/components/StructuresSponsored.vue
@@ -29,22 +29,6 @@
 	</div>
 </div>
 </div>
-  <!-- <div>
-    <h1>Strutture Sponsorizzate</h1>
-
-      <div class="card-deck">
-        <div class="card mycard my-4" v-for="structure in this.StructuresSponsored" :key="structure.id">
-          <img v-if="structure.cover_img_path" class="card-img-top myimg" :src="`asset(storage/ ${structure.cover_img_path})`" alt="Cover of structure">
-            <div class="card-body">
-              <h5 class="mt-0">{{structure.name}}</h5>
-            </div>
-            <div class="card-footer text-center">
-              <a class="btn btn-outline-primary my-1" :href="`http://127.0.0.1:8000/details?slug=${structure.slug}&contactedStructure=0`" role="button">Dettagli...</a><br>
-            </div>
-        </div>
-      </div>
-      
-  </div> -->
 </template>
 <script>
 export default {
